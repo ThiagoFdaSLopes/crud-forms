087951c62c2838c8ad062151b9a5b162ebd8af73
Thiago <programadorthiagolopes@gmail.com>
2023-01-02 17:45:17 -0300

    chore(route): Criado rota post

--- src/db/usersDB.js
+++ src/db/usersDB.js
@@ -1,7 +1,7 @@
 const conn = require('./connection');
 
 const insert = (people) => conn.execute(
-  'INSERT INTO users (first_name, last_name, user_password, email) VALUES(?, ?, ?, ?)',
+  'INSERT INTO crudforms.users (first_name, last_name, user_password, email) VALUES(?, ?, ?, ?)',
   [people.firstName, people.lastName, people.password, people.email],
 );
 
--- src/routes/routesCrudForms.js
+++ src/routes/routesCrudForms.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { getUsers } = require('../db/usersDB');
+const { getUsers, insert } = require('../db/usersDB');
 
 const router = express.Router();
 
+router.post('/user', async (req, res) => {
+  const [result] = await insert(req.body);
+
+  res.status(200).json(result);
+});
+
 router.get('/users', async (req, res) => {
-  const [users] = await getUsers();
-  res.status(200).json(users);
+  const [result] = await getUsers();
+  res.status(200).json(result);
 });
 
 module.exports = router;

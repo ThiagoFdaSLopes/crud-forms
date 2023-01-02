82489263b479bd1d444e03a2c5e2e1463f8e287e
Thiago <programadorthiagolopes@gmail.com>
2023-01-02 16:29:09 -0300

    chore(db): Criado functions para atribuir no mysql

---
+++ src/db/usersDB.js
@@ -0,0 +1,15 @@
+const conn = require('./connection');
+
+const insert = (people) => conn.execute(
+  'INSERT INTO users (first_name, last_name, user_password, email) VALUES(?, ?, ?, ?)',
+  [people.firstName, people.lastName, people.password, people.email],
+);
+
+const getUsers = () => conn.execute(
+  'SELECT * FROM users',
+);
+
+module.exports = {
+  getUsers,
+  insert,
+};

c71e6c6dfcf6e8213efb763a9d34c07057345dd1
Thiago <programadorthiagolopes@gmail.com>
2023-01-03 08:55:15 -0300

    chore: Passado middlewares para a rota post

---
+++ src/middlewares/index.js
@@ -0,0 +1,11 @@
+const checkName = require('./checkName');
+const checkEmail = require('./checkEmail');
+const checkLastName = require('./checkLastName');
+const checkPassword = require('./checkPassword');
+
+module.exports = {
+  checkEmail,
+  checkLastName,
+  checkName,
+  checkPassword,
+};
--- src/routes/routesCrudForms.js
+++ src/routes/routesCrudForms.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { getUsers, insert } = require('../db/usersDB');
+const { checkName, checkPassword, checkEmail, checkLastName } = require('../middlewares');
 
 const router = express.Router();
 
-router.post('/user', async (req, res) => {
+router.post('/user', checkName, checkLastName, checkEmail, checkPassword, async (req, res) => {
   const [result] = await insert(req.body);
 
   res.status(200).json(result);
